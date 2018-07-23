7adedaef682ea622b881dfe2d7f240c9a7af9a14
Robby Tarigan <robbytarigan@hotmail.com>
2018-07-23 23:10:40 +0100

    Folders

---
+++ CSharp/src/SimpleAlgorithm.Tests/FoldersTests.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+
+namespace SimpleAlgorithm.Tests
+{
+    public class FoldersTests
+    {
+        [Fact]
+        public void Should_return_correct_folder_names()
+        {
+            string xml =
+                "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
+                "<folder name=\"c\">" +
+                "<folder name=\"program files\">" +
+                "<folder name=\"uninstall information\" />" +
+                "</folder>" +
+                "<folder name=\"users\" />" +
+                "</folder>";
+
+            var expectedFolders = new[]
+            {
+                "uninstall information", "users"
+            };
+
+            var resultFolders = Folders.FolderNames(xml, 'u');
+
+            Assert.Equal(expectedFolders, resultFolders);
+        }
+    }
+}
---
+++ CSharp/src/SimpleAlgorithm/Folders.cs
@@ -0,0 +1,46 @@
+﻿/**
+ * Implement a function FolderNames, which accepts a string containing an XML file that specifies folder structure and returns all folder names that start with startingLetter. The XML format is given in the example below.
+ * For example, for the letter 'u' and XML file:
+ * <?xml version="1.0" encoding="UTF-8"?>
+ * <folder name="c">
+ *     <folder name="program files">
+ *         <folder name="uninstall information" />
+ *     </folder>
+ *     <folder name="users" />
+ * </folder>
+ * the function should return "uninstall information" and "users" (in any order).
+ */
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace SimpleAlgorithm
+{
+    public class Folders
+    {
+        public static IEnumerable<string> FolderNames(string xml, char startingLetter)
+        {
+            var xmldoc = XDocument.Parse(xml);
+
+            var folders = xmldoc.Descendants("folder").Where(e => e.Attribute("name")?.Value[0] == startingLetter);
+
+            return folders.Select(e => e.Attribute("name")?.Value);
+        }
+
+        //public static void Main(string[] args)
+        //{
+        //    string xml =
+        //        "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
+        //        "<folder name=\"c\">" +
+        //        "<folder name=\"program files\">" +
+        //        "<folder name=\"uninstall information\" />" +
+        //        "</folder>" +
+        //        "<folder name=\"users\" />" +
+        //        "</folder>";
+
+        //    foreach (string name in Folders.FolderNames(xml, 'u'))
+        //        Console.WriteLine(name);
+        //}
+    }
+}
