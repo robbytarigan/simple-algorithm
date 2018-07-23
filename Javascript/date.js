76d0a9b85bd9070a6a8c83f949a9975f56abfae8
Robby Tarigan <robbytarigan@hotmail.com>
2018-07-23 22:38:12 +0100

    Put description into date file.

--- Javascript/date.js
+++ Javascript/date.js
@@ -1,3 +1,8 @@
+/**
+ * Write a function that converts user entered date formatted as M/D/YYYY to a format required by an API (YYYYMMDD). The parameter "userDate" and the return value are strings.
+ * For example, it should convert user entered date "12/31/2014" to "20141231" suitable for the API.
+ */
+
 function formatDate(userDate) {
   // format from M/D/YYYY to YYYYMMDD
   const dateParts = userDate.split('/');
