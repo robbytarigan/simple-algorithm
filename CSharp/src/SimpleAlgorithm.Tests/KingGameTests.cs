6ad4ff4ed8e18532dbf983baae25f743a84258c6
Robby Tarigan <robbytarigan@hotmail.com>
2018-07-25 23:17:01 +0100

    Find which prisoner number getting released from King's game.

---
+++ CSharp/src/SimpleAlgorithm.Tests/KingGameTests.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+
+namespace SimpleAlgorithm.Tests
+{
+    public class KingGameTests
+    {
+        private KingGame _game;
+
+        public KingGameTests()
+        {
+            _game = new KingGame();
+        }
+
+        [Theory]
+        [InlineData(3, 3, 2)]
+        [InlineData(10, 1, 10)]
+        [InlineData(6, 2, 5)]
+        public void ReleasedNumber_should_give_correct_number(short p, short w, short expected)
+        {            
+            var result = _game.ReleasedNumber(p, w);
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ CSharp/src/SimpleAlgorithm/KingGame.cs
@@ -0,0 +1,40 @@
+﻿/**
+ * As prisoners, I want to be the last one that to be escaped from King game execution. The executioner always says some repeated words from the left to right before shooting the one it finished the word with.
+ * For example if the word is "MINI MANY MO" and there are four prisoners, then the third one is getting executed first. This continues until the last one that end up being released. 
+ */
+
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace SimpleAlgorithm
+{    
+    public class KingGame
+    {
+        public short ReleasedNumber(short prisonersCount, short wordsCount)
+        {
+            LinkedList<short> prisoners = new LinkedList<short>();
+
+            for (short p = 1; p < prisonersCount + 1; p++)
+            {
+                prisoners.AddLast(p);
+            }
+
+            var aimedPrisoner = prisoners.First;
+            
+            for (short p = 0; p < prisonersCount - 1; p++)
+            {
+                for (short w = 0; w < wordsCount - 1; w++)
+                {
+                    aimedPrisoner = aimedPrisoner.Next ?? prisoners.First;
+                }
+
+                var nextAimedPrisoner = aimedPrisoner.Next ?? prisoners.First;
+                prisoners.Remove(aimedPrisoner);
+                aimedPrisoner = nextAimedPrisoner;
+            }
+
+            Debug.Assert(prisoners.Count == 1);
+            return prisoners.First.Value;
+        }
+    }
+}
